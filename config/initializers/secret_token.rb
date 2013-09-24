2a63a58d36138b52471b403e5f8918ba16de8ea0
Edward Loveall <edward@edwardloveall.com>
2013-09-23 21:16:27 -0400

    Initial commit – empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Linkybits::Application.config.secret_key_base = '0271184fe0f0d440a29fd786d5dc6f7274120a7e3f3fb270c1b9345ed133db37233f0238dd5c76f233fc815c4d028d526ae26c9df0eb94ae2c628d1a38473b10'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Linkybits::Application.config.session_store :cookie_store, key: '_linkybits_session'
